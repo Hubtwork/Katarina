08e37e0be0f968e67846c6c1c73dca677e4db10b
Hubtwork <hubtwork@gmail.com>
2021-02-19 14:03:16 +0900

    master : init ios app and gitignore added
    ADD ios-SwiftUI APP Initialization
    ADD GitIgnore Settings ( swift, xcode )
    
    TODO - IOS Implementation

---
+++ katarina-ios/Katarina/Katarina/KatarinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KatarinaApp.swift
+//  Katarina
+//
+//  Created by Jae Heo on 2021/02/19.
+//
+
+import SwiftUI
+
+@main
+struct KatarinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
